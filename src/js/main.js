99935487164d198aabf81b92dff909494c39bbc9
Caropox <54894433+Caropox@users.noreply.github.com>
2019-10-03 15:22:19 +0200

    modification bouton dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -26,10 +26,32 @@ document.addEventListener("DOMContentLoaded", async function(event) {
 Fonction qui appelle l'API 
 */
 async function callAPI(){
-    let articles = await fetch('https://newsapi.org/v2/top-headlines?q=environment&apiKey=2f8847d9e4e1440ea93c57a8d9539684');
+    let articles = await fetch('https://newsapi.org/v2/top-headlines?q=pollution&apiKey=2f8847d9e4e1440ea93c57a8d9539684');
     console.log(articles)
     return await articles.json();
 }
 
 console.log(callAPI());
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Gestion du menu mobile
+    document.getElementById("nav-mobile-open").addEventListener("click", openNavMenu, false);
+    document.getElementById("nav-mobile-close").addEventListener("click", closeNavMenu, false);
+
+    var mobileMenuLinks = document.querySelectorAll("nav#nav-mobile ul li a");
+    mobileMenuLinks.forEach(function(linkElem) {
+        linkElem.addEventListener("click", closeNavMenu, false);
+    })
+
+    function openNavMenu() {
+        var menuToEdit = document.getElementById("mobileMenu");
+        menuToEdit.classList.remove("hidden"); // EnlÃ¨ve la classe CSS
+        menuToEdit.removeAttribute("hidden"); // EnlÃ¨ve l'attribut HTML
+    }
+
+    function closeNavMenu() {
+        var menuToEdit = document.getElementById("mobileMenu");
+        menuToEdit.classList.add("hidden"); // Ajoute la classe CSS
+        menuToEdit.setAttribute("hidden","hidden"); // Ajoute l'attribut HTML
+    }
+});
